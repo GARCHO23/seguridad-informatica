4df046266c468738504c12ee5543571b84805d85
dirudab0 <dirudab@gmail.com>
2016-10-24 22:34:47 -0500

    Modificaciones
    Cambios al pagina de autores

--- js/WModal.js
+++ js/WModal.js
@@ -21,18 +21,20 @@ function currentDiv(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
+  var y = document.getElementsByClassName("dato");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
      x[i].style.display = "none";
+     y[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
      dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-
   }
   x[slideIndex-1].style.display = "block";
+  y[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-opacity-off";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
